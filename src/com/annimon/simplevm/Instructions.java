106546074efeece74ad4bfb829cfaecdbcc0eaae
Victor <melnikukr@mail.ru>
2015-02-14 13:27:15 +0200

    Add Instruction opcodes

---
+++ src/com/annimon/simplevm/Instructions.java
@@ -0,0 +1,32 @@
+package com.annimon.simplevm;
+
+/**
+ *
+ * @author aNNiMON
+ */
+public class Instructions {
+    
+    public static final byte
+            ILOAD = 10,
+            ISTORE = 11,
+            
+            IADD = 40, // n + m
+            ISUB = 41, // n - m
+            IMUL = 42, // n * m
+            IDIV = 43, // n / m
+            INEG = 44, // -n
+            IINC = 45, // n++
+            IDEC = 46, // n--
+            
+            IFEQ = 80, // ==
+            IFNE = 81, // !=
+            IFGE = 82, // >=
+            IFGT = 83, // >
+            IFLE = 84, // <=
+            IFLT = 85, // <
+            
+            GOTO = 120,
+            
+            NOP = 0;
+            
+}
